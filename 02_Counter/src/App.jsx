ba1c66e5c2e857cb0a47f8c07e58f85ed3e3a6b3
Mohit Bhalotia <mohitkumarbhalotia420@gmail.com>
2024-12-24 16:37:07 +0530

    Counter done

---
+++ 02_Counter/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <div>
+        <h1>Chai aur React</h1>
+        <h2>Counter Value : {counter}</h2>
+      </div>
+      <button disabled={counter <= 0} onClick={() => setCounter(counter - 1)}>
+        Decrease Value
+      </button>
+      <button disabled={counter >= 20} onClick={() => setCounter(counter + 1)}>
+        Increase Value
+      </button>
+    </div>
+  );
+};
+
+export default App;

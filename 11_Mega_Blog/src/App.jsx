f2992baf4dc87b4d200f98a2f4f9662cb4c57f81
Mohit Bhalotia <mohitkumarbhalotia420@gmail.com>
2025-01-09 04:57:42 +0530

    Modified Suspense

--- 11_Mega_Blog/src/App.jsx
+++ 11_Mega_Blog/src/App.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { login, logout } from "./store/slices/authSlice";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
       <main className="flex-grow w-full container mx-auto px-4 py-8 ">
-        <Outlet />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
       </main>
       <Footer />
     </div>
